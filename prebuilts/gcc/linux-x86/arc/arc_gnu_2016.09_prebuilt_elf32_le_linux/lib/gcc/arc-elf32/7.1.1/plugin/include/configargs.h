6dc941a2b109b32dc9a50e9cf01d81ac68a5f339
chen <693191690@qq.com>
2021-08-04 10:07:58 +0800

    add prebuilts/gcc/

---
+++ prebuilts/gcc/linux-x86/arc/arc_gnu_2016.09_prebuilt_elf32_le_linux/lib/gcc/arc-elf32/7.1.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/SCRATCH/arcjenkins/custom-ru20-arcgnu2/workspace/arc_gnu_toolchain_release/gcc/configure --target=arc-elf32 --with-cpu=arc700 --with-pkgversion='ARCompact/ARCv2 ISA elf32 toolchain 2017.09' --with-bugurl=https://github.com/foss-for-synopsys-dwc-arc-processors/toolchain/issues --enable-fast-install=N/A --with-endian=little --disable-werror --enable-languages=c,c++ --disable-shared --disable-tls --disable-threads --prefix=/SCRATCH/arcjenkins/custom-ru20-arcgnu2/workspace/arc_gnu_toolchain_release/toolchain/../release_output/arc_gnu_2017.09_prebuilt_elf32_le_linux_install --with-gnu-as --with-gnu-ld --with-python=no --with-newlib --with-headers=/SCRATCH/arcjenkins/custom-ru20-arcgnu2/workspace/arc_gnu_toolchain_release/toolchain/../release_output/arc_gnu_2017.09_prebuilt_elf32_le_linux_install/arc-elf32/include --with-libs=/SCRATCH/arcjenkins/custom-ru20-arcgnu2/workspace/arc_gnu_toolchain_release/toolchain/../release_output/arc_gnu_2017.09_prebuilt_elf32_le_linux_install/arc-elf32/lib 'CFLAGS_FOR_TARGET=-ffunction-sections -fdata-sections '";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "arc700" } };
---
+++ prebuilts/gcc/linux-x86/arm/arm-eabi-4.9/lib/gcc/arm-eabi/4.9.4/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-4.9.4/configure --prefix=/home/zhoubo/develop/toolchains/trusty/trusty_toolchain --target=arm-eabi --enable-languages=c,c++ --disable-multilib : (reconfigured) ../gcc-4.9.4/configure --prefix=/home/zhoubo/develop/toolchains/trusty/trusty_toolchain --target=arm-eabi --enable-languages=c,c++ --disable-multilib --enable-libgomp : (reconfigured) ../gcc-4.9.4/configure --prefix=/home/zhoubo/develop/toolchains/trusty/trusty_toolchain --target=arm-eabi --enable-languages=c,c++ --disable-multilib --enable-libgomp --enable-threads=posix : (reconfigured) ../gcc-4.9.4/configure --prefix=/home/zhoubo/develop/toolchains/trusty/trusty_toolchain --target=arm-eabi --enable-languages=c,c++ --disable-multilib --enable-libgomp";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
