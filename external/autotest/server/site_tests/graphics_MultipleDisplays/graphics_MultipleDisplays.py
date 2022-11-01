f2b141f5fe175c0f9ba524553b7281d64cf89d9f
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-01 12:40:25 +0000

    Adding tarfile member sanitization to extractall()

--- external/autotest/server/site_tests/graphics_MultipleDisplays/graphics_MultipleDisplays.py
+++ external/autotest/server/site_tests/graphics_MultipleDisplays/graphics_MultipleDisplays.py
@@ -85,7 +85,26 @@ class graphics_MultipleDisplays(test.test):
             archive_path = os.path.join(self.bindir, 'files', static_content)
 
             with tarfile.open(archive_path, 'r') as tar:
-                tar.extractall(temp_bundle_dir)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, temp_bundle_dir)
 
         # Send bundle to client. The extra slash is to send directory contents.
         self._host.run('mkdir -p {}'.format(self.CLIENT_TEST_ROOT))
